7de5d0c47c315ab69739b4bc8d80ad947491b613
kavindunavod <140165157+kavindunavod@users.noreply.github.com>
2024-06-15 00:19:52 +0530

    total time to go home

---
+++ project8.scala
@@ -0,0 +1,17 @@
+object project8 {
+
+  def main(args: Array[String]): Unit = {
+
+    calculate();
+  }
+
+  def calculate(): Unit = {
+    val first = 8*2;
+    val second = 7*3;
+    val third = 2*8;
+    val total = first + second + third;
+    println("total time to go to the home: " +total);
+
+  }
+
+}
